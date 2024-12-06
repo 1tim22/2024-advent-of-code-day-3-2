df45417aa0106e80b64e1a7813d2b41744bb5e42
Andrew Baron <abaron@sbts.edu>
2024-12-06 08:28:33 -0500

    Adds first puzzle solution
    Updates parse_operation with RegEx to remove disabled content
    
    Answer submitted:
    56372201
    
    Response:
    That's not the right answer; your answer is too low.
    
    To the King of the ages, immortal, invisible, the only God, be honor and glory forever and ever. Amen.

--- tests/test_example_input.rs
+++ tests/test_example_input.rs
@@ -4,11 +4,11 @@ use day_3::*;
 #[ignore]
 fn test_parse_and_multiplication() {
     let table: Vec<(String, i32, i32)> = parse_operation(
-        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string(),
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string(),
         r"\w\w\w"
     );
 
     let total = process(table);
 
-    assert_eq!(total, 161);
+    assert_eq!(total, 48);
 }
